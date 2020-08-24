18dc38f4eac332435c978557ffb00e4fa2a27671
kartikchauhan <chauhan.kartik25@gmail.com>
2020-08-23 18:59:17 -0700

    add description for the functions used

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 (async () => {
-  const codePromise = fetch('../out/main.wasm');
-  const { instance } = await WebAssembly.instantiateStreaming(codePromise);
-  
-  console.log(instance);
-  const buffer = new Uint8Array(instance.exports.memory.buffer);
-  const functionPointer = instance.exports.helloWorld();
+	const codePromise = fetch('../out/main.wasm');
 
-  let str = "";
+	// `instantiateStreaming` function compiles and instantiates a WebAssembly module directly from a streamed underlying source.
+	// This is the most efficient, optimized way to load wasm code.
+	// Source: A response object or a promise that will fulfill with one, representing the underlying source of a .wasm module you want to stream, compile and instantiate.
+	// Return Value:
+	// A Promise that resolves to a ResultObject which contains two fields:
+	//   module: A WebAssembly.Module object representing the compiled WebAssembly module.
+	//   instance: A WebAssembly.Instance object that contains all the Exported WebAssembly functions.
 
-  for(let i = functionPointer; buffer[i]; i++) {
-    str += String.fromCharCode(buffer[i]);
-  }
+	const { instance } = await WebAssembly.instantiateStreaming(codePromise);
 
-  document.getElementById('container').textContent = str;
+	const buffer = new Uint8Array(instance.exports.memory.buffer);
+	const helloWorldPointer = instance.exports.helloWorld();
+
+	let str = "";
+
+	for (let i = helloWorldPointer; buffer[i]; i++) {
+		str += String.fromCharCode(buffer[i]);
+	}
+
+	document.getElementById('container').textContent = str;
 })();
